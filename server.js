ccf8f2794f802d1fb0f59df0868245cd4598b55d
NamanMeravi <namanmeravi0@gmail.com>
2025-10-28 01:46:45 +0530

    added

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 // ✅ Proper CORS setup
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin:"http://localhost:5173",
     credentials: true,
   })
 );
